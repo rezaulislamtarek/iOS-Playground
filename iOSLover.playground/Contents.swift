6de90bd990f128f1da696c4a0000cbe907f28a8f
rezaulislam <mrezaulislam.t@gmail.com>
2024-06-10 08:40:54 +0600

    What is the difference between a variable, optional and Implicitly unwrapped optioanl?

--- iOSLover.playground/Contents.swift
+++ iOSLover.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// What is the difference between a variable, optional and Implicitly unwrapped optioanl?
+
+///
+    var a : String
+    var b : String? // Optional
+    var c : String! // Implicitly unwrapped optional
+///
+
+b = nil
+
+print("c's value is \(b)")
