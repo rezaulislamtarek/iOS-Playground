5628f0a8ccb4560c84b24ae72964b91332e5f27b
rezaulislam <mrezaulislam.t@gmail.com>
2024-06-23 22:24:33 +0600

    ARC : Strong Reference

--- iOSLover.playground/Contents.swift
+++ iOSLover.playground/Contents.swift
@@ -1,15 +1,24 @@
 import UIKit
-/*
- Capture list in closures
- */
 
 
-var userName : String = "Rezaul"
-
-let code = { [userName] in
-    print(userName)
+class Person{
+    var name : String
+    
+    init(name: String) {
+        self.name = name
+        print("\(name) is initialized")
+    }
+    
+    deinit{
+        print("\(name) is deinitialized")
+    }
 }
 
-userName = "Tarek"
+var p1 : Person? = Person(name: "Rezaul") // Chank of memory allocate ARC = 1
+var p2 = p1   // ARC = 1 + 1 = 2
 
-code()
+p1 = nil    // ARC = 2 - 1 = 1
+print(p1?.name) // nil
+print(p2?.name) // Rezaul
+p2 = nil        // ARC = 1 - 1 = 0 // Rezaul is deinitialized
+print(p2?.name) // nil
