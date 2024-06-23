edae4c9899d7aff16d8ec4142e38b45d79b7d9da
rezaulislam <mrezaulislam.t@gmail.com>
2024-06-23 23:22:07 +0600

    Strong and weak reference

--- iOSLover.playground/Contents.swift
+++ iOSLover.playground/Contents.swift
@@ -1,24 +1,47 @@
 import UIKit
 
-
-class Person{
+class Person {
     var name : String
+    var kutta : Kutta?
     
-    init(name: String) {
+    init(name: String ) {
         self.name = name
-        print("\(name) is initialized")
+        
     }
+}
+ 
+class Kutta{
+    var name : String
+    weak var owner : Person?
     
+    init(name: String, owner: Person) {
+        self.name = name
+        self.owner = owner
+    }
     deinit{
-        print("\(name) is deinitialized")
+        print("Kuttar nam \(name)")
     }
 }
 
-var p1 : Person? = Person(name: "Rezaul") // Chank of memory allocate ARC = 1
-var p2 = p1   // ARC = 1 + 1 = 2
+var person : Person? = Person(name: "Khan Shab")  // Persons ARC = 1
+var kutta  : Kutta? = Kutta(name: "Sparkiiii", owner: person!)  // Kuttas ARC = 1
 
-p1 = nil    // ARC = 2 - 1 = 1
-print(p1?.name) // nil
-print(p2?.name) // Rezaul
-p2 = nil        // ARC = 1 - 1 = 0 // Rezaul is deinitialized
-print(p2?.name) // nil
+person?.kutta = kutta   // Kuttas ARC = 2
+kutta?.owner = person   // Persons ARC = ? 1
+
+print(kutta?.owner?.name)    // Khan Shab
+
+person = nil
+
+print(kutta?.owner?.name)    //nil
+
+
+
+
+/*
+    Memory block for Person ; ARC for Person = 1
+    Memory block for Kutta ; ARC for Kutta = 1
+    person.kutta = kutta ; ARC for kutta = 1 + 1 = 2
+ 
+ */
+
