3c1279608381a0509bff8462ca561792293c3bfd
rezaulislam <mrezaulislam.t@gmail.com>
2024-06-30 21:35:17 +0600

    Custom publisher which emit a value to its subscriver

--- iOSLover.playground/Contents.swift
+++ iOSLover.playground/Contents.swift
@@ -1,6 +1,28 @@
 import Foundation
 import Combine
 
+struct Just<T> : Publisher{
+    typealias Output = T
+    typealias Failure = Never
+    
+    private let value : T
+    init(_ value  : T){
+        self.value = value
+    }
+    func receive<S>(subscriber: S) where S : Subscriber, Never == S.Failure, T == S.Input {
+        let _ = subscriber.receive(self.value)
+        subscriber.receive(completion: .finished)
+    }
+}
+
+let _ = Just("Rezaul").sink { completion in
+    print(completion)
+} receiveValue: { value in
+    print(value)
+}
+
+
+/*
 struct Empty : Publisher{
     typealias Output = Never
     
@@ -18,4 +40,4 @@ let _ = Empty().sink { completion in
 } receiveValue: { _ in
     
 }
-
+*/
