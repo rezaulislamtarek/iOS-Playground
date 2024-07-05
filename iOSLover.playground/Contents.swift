e3a9f3fa00cadd6a7d2f99f23565d9c63556c09e
rezaulislam <mrezaulislam.t@gmail.com>
2024-07-05 10:31:41 +0600

    Lazy var

--- iOSLover.playground/Contents.swift
+++ iOSLover.playground/Contents.swift
@@ -1,48 +1,44 @@
 import Foundation
 
-func caloriesBurntByWalking(distance : Double) -> Double {
-    return distance * 1.5
-}
-func caloriesBurntByJogging(distance : Double) -> Double {
-    return distance * 2.5
-}
-func caloriesBurntBySkipping(distance : Double) -> Double {
-    return distance * 3.5
-}
-
-//closure
-let burntByFasting : (Double) -> Double = { hour in
-    return hour * 2
-}
-
-// High Order function
-func caloriesBurntByActivity( activity : (Double) -> Double, unit : Double) -> Double {
-    return activity(unit)
-}
-
-let res = caloriesBurntByActivity(activity: caloriesBurntBySkipping , unit: 2)
-print(res)
-
-
-let resFasting = caloriesBurntByActivity(activity: burntByFasting , unit: 14)
-print(resFasting)
-
-
-//closure
-var getDifFrom1970 : (Date) -> String  = { date in
-    return date.timeIntervalSince1970.description
-}
-
-print(getDifFrom1970(Date()))
-
-
-// High Order function
-func userStage(stage : Int) -> (Double)->Double {
-    if stage == 1 {
-        return burntByFasting
+class Smaple{
+    var initialValue : Int
+    init(initialValue: Int) {
+        self.initialValue = initialValue
     }
-    return caloriesBurntByJogging
+    
+    func update(value : Int){
+        self.initialValue = value
+    }
+    
+    //Computed property
+    var someComputedProperty: String {
+        if self.initialValue > 10 {
+            //print("GREATER THAN 10")
+            return "GREATER THAN 10"
+        } else {
+           // print("A SINGLE DIGIT NUMBER")
+            return "A SINGLE DIGIT NUMBER"
+        }
+    }
+    
+    //Lazy prperty
+    lazy var someLazyProperty : String = "Initial value is \(initialValue)"
+    
 }
 
- let function = userStage(stage: 2)
-print(function(5))
+/*
+    Lazy prperty captured 1st times accessed values.
+    As a result it shows initial value is 5 when it call second time.
+ */
+
+
+let sample = Smaple(initialValue: 5)
+ 
+sample.someLazyProperty
+sample.someComputedProperty
+sample.update(value: 20)
+sample.someLazyProperty
+sample.someComputedProperty
+
+
+
